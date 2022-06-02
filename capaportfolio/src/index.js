f672efb0b7ed3dc0f8ad693da94c7ffda44604f4
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-06-02 10:22:36 -0300

    Arrumando arquivos

--- capaportfolio/src/index.js
+++ capaportfolio/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/* Importar o app */
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* Capturando o root do html em uma variavel */
+const root = document.getElementById("root");
+
+/* Renderizando o projeto */
+ReactDOM.render(<App />, root);
